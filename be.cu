5396ae1550034b69af8f0ef3737e2d665a47297d
Filippo Biondi <filippo22@ru.is>
2024-12-10 16:02:47 +0100

    Sync

--- be.cu
+++ be.cu
@@ -5,28 +5,29 @@
 #include "device_launch_parameters.h"
 
 #define N 11
+#define EDGES 20
 
 void checkCUDAError(const char*);
 void getMatrix(int* m, int n_nodes);
 
 int main(void) {
-	int *matrix;			                   
-	int *d_matrix, *d_nodes, *d_weights;
-	unsigned int matrixSize = N * N * sizeof(int);
-	unsigned int nodeSize = N * sizeof(int);
+    long int edge_index[2][20] = {
+        {0,1,2,3,0,0,1,4,1,5,1,6,2,7,2,8,3,9,3,10},
+        {1,0,0,0,2,3,4,1,5,1,6,1,7,2,8,2,9,3,10,3}
+    };
+	long int *d_index_a, *d_index_b, *d_nodes, *d_weights;
+	unsigned int nodeSize = N * sizeof(long int);
+	unsigned int edgeSize = EDGES * sizeof(long int);
 
-    matrix = (int*)malloc(matrixSize);
-    getMatrix(matrix, N);
-
-	cudaMalloc((void **)&d_matrix, matrixSize);
+	cudaMalloc((void **)&d_index_a, edgeSize);
+	cudaMalloc((void **)&d_index_b, edgeSize);
 	cudaMalloc((void **)&d_nodes, nodeSize);
 	cudaMalloc((void **)&d_weights, nodeSize);
 	checkCUDAError("CUDA malloc");
 
-	cudaMemcpy(d_matrix, matrix, matrixSize, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_index_a, edge_index[0], edgeSize, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_index_b, edge_index[1], edgeSize, cudaMemcpyHostToDevice);
 	checkCUDAError("CUDA memcpy");
-
-
 
 	return 0;
 }
@@ -39,18 +40,4 @@ void checkCUDAError(const char *msg)
 		fprintf(stderr, "CUDA ERROR: %s: %s.\n", msg, cudaGetErrorString(err));
 		exit(EXIT_FAILURE);
 	}
-}
-
-void getMatrix(int* m, int n_nodes)
-{
-    int edge_index[2][20] = {
-        {0,1,2,3,0,0,1,4,1,5,1,6,2,7,2,8,3,9,3,10},
-        {1,0,0,0,2,3,4,1,5,1,6,1,7,2,8,2,9,3,10,3}
-    };
-    int l_index = 20;
-
-    for(int i=0; i< l_index; ++i) {
-        m[(edge_index[0][i] * n_nodes) + edge_index[1][i]] = 1;
-        m[(edge_index[1][i] * n_nodes) + edge_index[0][i]] = 1;
-    }
 }
