506c265d0f00e61b7c57bf23ecbf8468fb64399d
Filippo Biondi <filippo22@ru.is>
2024-12-11 23:46:18 +0100

    update

--- be.cu
+++ be.cu
@@ -4,78 +4,204 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-#define N 11
-#define EDGES 20
+#define THREAD_N 256
 
 void checkCUDAError(const char*);
+int* read_file_graph(int* edge_n, int* node_n);
+int read_file_int(FILE *file);
 
-__global__ void compute_weights (
-    int *edge_start,
-    int *edge_end,
-    int *weights,
-    int *node_blocks,
-    int *splitters,
-    int *splitters_mask,
-    int *current_splitter_index
-) {
+__global__ void compute_weights(int *edge_start, int *edge_end, int *edge_n, int *weights, int *node_blocks, int *splitters, int *splitters_mask, int *current_splitter_index) {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if(i < EDGES) {
+    if(i < (*edge_n)) {
+        int es = edge_end[i];
+        int block = node_blocks[es];
+        int splitter = splitters[*current_splitter_index];
         atomicAdd(
-            &weights[edge_end[i]],
-            node_blocks[edge_start[i]] == splitters[*current_splitter_index]
+            &weights[edge_start[i]],
+            block == splitter
         );
         splitters_mask[splitters[*current_splitter_index]] = 0;
     }
 }
 
-int main(void) {
-    int edge_index[2][20] = {
-        {0,1,2,3,0,0,1,4,1,5,1,6,2,7,2,8,3,9,3,10},
-        {1,0,0,0,2,3,4,1,5,1,6,1,7,2,8,2,9,3,10,3}
-    };
-    int *weights, *current_splitter_index;
-	int *d_edge_start, *d_edge_end, *d_node_blocks, *d_weights,
-        *d_splitters, *d_current_splitter_index, *d_splitters_mask;
-	unsigned int nodeSize = N * sizeof(int);
-	unsigned int edgeSize = EDGES * sizeof(int);
+__global__ void compute_max_node_w(int *weights, int *max_node_w, int *node_n) {
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if(i < (*node_n)) {
+        atomicMax(max_node_w, weights[i]);
+    }
+}
 
-	weights = (int *)malloc(nodeSize);
-	current_splitter_index = (int *)malloc(sizeof(int));
-    *current_splitter_index = 0;
+__global__ void init_ballot(int *node_blocks, int *max_node_w, int *weights, int *weight_adv, int *node_n) {
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if(i < (*node_n)) {
+        weight_adv[(*max_node_w) * node_blocks[i] + weights[i]] = (*node_n);
+    }
+}
 
-	cudaMalloc((void **)&d_edge_start, edgeSize);
-	cudaMalloc((void **)&d_edge_end, edgeSize);
-	cudaMalloc((void **)&d_node_blocks, nodeSize);
-	cudaMalloc((void **)&d_weights, nodeSize);
-	cudaMalloc((void **)&d_splitters, nodeSize);
-	cudaMalloc((void **)&d_splitters_mask, nodeSize);
-	cudaMalloc((void **)&d_current_splitter_index, sizeof(int));
-	checkCUDAError("CUDA malloc");
+__global__ void block_ballot(int *node_blocks, int *max_node_w, int *weights, int *weight_adv, int *node_n) {
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if(i < (*node_n)) {
+        atomicMin(
+            &weight_adv[(*max_node_w) * node_blocks[i] + weights[i]],
+            i
+        );
+    }
+}
 
-	cudaMemcpy(d_edge_start, edge_index[0], edgeSize, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_edge_end, edge_index[1], edgeSize, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_current_splitter_index, current_splitter_index, sizeof(int), cudaMemcpyHostToDevice);
-	checkCUDAError("CUDA memcpy");
+__global__ void split(int *new_node_blocks, int *node_blocks, int *max_node_w, int *weights, int *weight_adv, int *node_n) {
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if(i < (*node_n)) {
+        new_node_blocks[i] = weight_adv[(*max_node_w) * node_blocks[i] + weights[i]];
+        weights[i] = 0;
+    }
+}
 
-
-    while(*current_splitter_index >= 0) {
-        cudaMemcpy(current_splitter_index, d_current_splitter_index, sizeof(int), cudaMemcpyDeviceToHost);
-        *current_splitter_index = (*current_splitter_index) - 1;
-        cudaMemcpy(d_current_splitter_index, current_splitter_index, sizeof(int), cudaMemcpyHostToDevice);
-        checkCUDAError("CUDA memcpy");
-
-        compute_weights<<<(EDGES+255)/256, 256>>>(d_edge_start, d_edge_end, d_weights, d_node_blocks, d_splitters, d_splitters_mask, d_current_splitter_index);
-        checkCUDAError("Compute Weights");
-
-        cudaMemcpy(weights, d_weights, nodeSize, cudaMemcpyDeviceToHost);
-        checkCUDAError("CUDA memcpy");
-
-        for (int i =0; i< N; ++i){
-            printf("%d \n", weights[i]);
+__global__ void add_splitters(int *new_node_blocks, int *node_blocks, int *splitters, int *current_splitter_index, int *splitters_mask, int *node_n) {
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if(i < (*node_n) && i == new_node_blocks[i] && new_node_blocks[i] != node_blocks[i]) {
+        int new_splitter_index = atomicAdd(current_splitter_index, 1);
+        splitters[new_splitter_index] = i;
+        splitters_mask[i] = 1;
+        int old_block = node_blocks[i];
+        int old_splitted = atomicExch(&splitters_mask[old_block], 1);
+        if(!old_splitted) {
+            new_splitter_index = atomicAdd(current_splitter_index, 1);
+            splitters[new_splitter_index] = old_block;
         }
     }
+}
+
+int main(void) {
+    int node_n = 0;
+    int edge_n = 0;
+    int* edge_index = read_file_graph(&edge_n, &node_n);
+
+	int *d_edge_start, *d_edge_end, *d_edge_n, *d_node_n, *d_new_node_blocks,
+        *d_node_blocks,*d_current_splitter_index, *d_max_node_w, *d_weights, *d_splitters,
+        *d_splitters_mask,*d_weight_adv, *d_swap;
+
+	unsigned int node_size = node_n * sizeof(int);
+	unsigned int edge_size = edge_n * sizeof(int);
+
+	int *current_splitter_index = (int *)calloc(sizeof(int),0);
+	int *max_node_w = (int *)calloc(sizeof(int), 0);
+    int init = 0;
+
+	cudaMalloc((void **)&d_weights, node_size);
+	cudaMalloc((void **)&d_edge_n, sizeof(int));
+	cudaMalloc((void **)&d_node_n, sizeof(int));
+	cudaMalloc((void **)&d_edge_start, edge_size);
+	cudaMalloc((void **)&d_edge_end, edge_size);
+	cudaMalloc((void **)&d_new_node_blocks, node_size);
+	cudaMalloc((void **)&d_node_blocks, node_size);
+	cudaMalloc((void **)&d_splitters, node_size);
+	cudaMalloc((void **)&d_splitters_mask, node_size);
+	cudaMalloc((void **)&d_current_splitter_index, sizeof(int));
+	cudaMalloc((void **)&d_max_node_w, sizeof(int));
+	checkCUDAError("CUDA malloc");
+
+    cudaMemset(d_weights,0, node_size);
+    cudaMemset(d_edge_n,0, sizeof(int));
+    cudaMemset(d_node_n,0, sizeof(int));
+    cudaMemset(d_edge_start,0, edge_size);
+    cudaMemset(d_edge_end,0, edge_size);
+    cudaMemset(d_new_node_blocks,0, node_size);
+    cudaMemset(d_node_blocks,0, node_size);
+    cudaMemset(d_splitters,0, node_size);
+    cudaMemset(d_splitters_mask,0, node_size);
+    cudaMemset(d_current_splitter_index,0, sizeof(int));
+    cudaMemset(d_max_node_w,0, sizeof(int));
+    checkCUDAError("CUDA memset");
+
+	cudaMemcpy(d_edge_start, edge_index, edge_size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_edge_end, &edge_index[edge_n], edge_size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_edge_n, &edge_n, sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_node_n, &node_n, sizeof(int), cudaMemcpyHostToDevice);
+	checkCUDAError("CUDA memcpy 1");
+
+
+    while((*current_splitter_index) >= 0) {
+        compute_weights<<<(edge_n+(THREAD_N-1)) / THREAD_N, THREAD_N>>>(
+                d_edge_start,
+                d_edge_end,
+                d_edge_n,
+                d_weights,
+                d_node_blocks,
+                d_splitters,
+                d_splitters_mask,
+                d_current_splitter_index
+        );
+        checkCUDAError("Compute Weights");
+
+       if(!init) {
+            compute_max_node_w<<<(node_n+(THREAD_N-1)) / THREAD_N, THREAD_N>>>(d_weights, d_max_node_w, d_node_n);
+            checkCUDAError("Computing Max Weight");
+            cudaMemcpy(max_node_w, d_max_node_w, sizeof(int), cudaMemcpyDeviceToHost);
+            checkCUDAError("CUDA memcpy 2");
+	        cudaMalloc((void **)&d_weight_adv, (node_n*((*max_node_w)+1))*sizeof(int));
+	        checkCUDAError("CUDA malloc");
+            init = 1;
+        }
+
+        init_ballot<<<(node_n+(THREAD_N-1)) / THREAD_N, THREAD_N>>>(d_node_blocks, d_max_node_w, d_weights, d_weight_adv, d_node_n);
+        checkCUDAError("Init advert");
+
+        block_ballot<<<(node_n+(THREAD_N-1)) / THREAD_N, THREAD_N>>>(d_node_blocks, d_max_node_w, d_weights, d_weight_adv, d_node_n);
+        checkCUDAError("Block ballot");
+
+        split<<<(node_n+(THREAD_N-1)) / THREAD_N, THREAD_N>>>(d_new_node_blocks, d_node_blocks, d_max_node_w, d_weights, d_weight_adv, d_node_n);
+        checkCUDAError("Split");
+
+        add_splitters<<<(node_n+(THREAD_N-1)) / THREAD_N, THREAD_N>>>(d_new_node_blocks, d_node_blocks, d_splitters, d_current_splitter_index, d_splitters_mask, d_node_n);
+        checkCUDAError("Add splitters");
+
+        cudaMemcpy(current_splitter_index, d_current_splitter_index, sizeof(int), cudaMemcpyDeviceToHost);
+        checkCUDAError("CUDA memcpy 3");
+        *current_splitter_index = (*current_splitter_index) - 1;
+        cudaMemcpy(d_current_splitter_index, current_splitter_index, sizeof(int), cudaMemcpyHostToDevice);
+        checkCUDAError("CUDA memcpy 4");
+
+        d_swap = d_node_blocks;
+        d_node_blocks = d_new_node_blocks;
+        d_new_node_blocks = d_swap;
+    }
+    cudaDeviceSynchronize();
+
+    int *result = (int*)malloc(node_n * sizeof(int));
+    cudaMemcpy(result, d_node_blocks, node_size, cudaMemcpyDeviceToHost);
+    checkCUDAError("CUDA memcpy 5");
+
+    printf("[%d", result[0]);
+    for (int i=1; i<node_n; ++i) {
+        printf(",%d", result[i]);
+    }
+    printf("]");
 
 	return 0;
+}
+
+int* read_file_graph(int* edge_n, int* node_n) {
+    FILE *file = fopen("graph.txt", "r");
+    *node_n = read_file_int(file);
+    *edge_n = read_file_int(file);
+    int *edge_index = (int*)malloc(sizeof(int) * (*edge_n) * (*edge_n));
+    for(int i=0; i<(*edge_n); ++i) {
+        edge_index[i] = read_file_int(file); 
+        edge_index[(*edge_n) + i] = read_file_int(file); 
+    }
+    return edge_index;
+}
+
+int read_file_int(FILE *file) {
+    char ch = fgetc(file);
+    int n = 0;
+    int c = 0;
+    while(ch != ' ' && ch != '\n') {
+        c = ch - '0';   
+        n = (n*10) + c;
+        ch = fgetc(file);
+    }
+    return n;
 }
 
 void checkCUDAError(const char *msg)
